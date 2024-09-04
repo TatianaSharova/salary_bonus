3aae496b967704f080f84b458f4bf9b1a548444c
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-09-04 22:52:24 +0400

    add apscheduler

--- program.py
+++ program.py
@@ -1,171 +1,27 @@
-import time
-import os
 import asyncio
+import time
+
 import aiogram
-from aiogram.exceptions import TelegramAPIError
-from datetime import datetime as dt
-
-from dotenv import load_dotenv
-
 import gspread
 import pandas as pd
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from gspread.worksheet import Worksheet
 from gspread_formatting import *
 from pandas.core.frame import DataFrame
-from pandas.core.series import Series
 
+from counting_points import count_adjusting_points, count_points
 from exceptions import TooManyRequestsApiError
-from worksheets import (connect_to_project_archive, connect_to_settings_ws,
-                        send_quarter_data_to_spreadsheet,
+from quaterly_points import calculate_quarter
+from utils import TELEGRAM_TOKEN, get_list_of_engineers, is_point, send_message
+from worksheets import (connect_to_project_archive,
+                        send_adj_data_to_spreadsheet,
                         send_project_data_to_spreadsheet,
-                        send_adj_data_to_spreadsheet)
+                        send_quarter_data_to_spreadsheet)
 
 pd.options.mode.chained_assignment = None
-from counting_points import (check_amount_directions, check_authors,
-                             check_cultural_heritage, check_filled_projects,
-                             check_net, check_sot_skud, check_spend_time,
-                             check_square)
-from quaterly_points import calculate_quarter
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-env_path = os.path.join(BASE_DIR, '.env')
-load_dotenv(dotenv_path=env_path)
 
 
-creds_path = os.path.join(BASE_DIR, 'creds.json')
-
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-
-async def send_message(bot: aiogram.Bot, message: str):
-    '''Присылает сообщение в телеграме.'''
-    try:
-        await bot.send_message(chat_id=TELEGRAM_CHAT_ID,
-                               text=f'{message}')
-    except TelegramAPIError:
-        pass
-
-def non_count_engineers() -> list[str]:
-    '''
-    Получает список проектировщиков, для которых не надо делать премиальный расчет.
-    '''
-    sheet = connect_to_settings_ws()
-    
-    df = pd.DataFrame(sheet.get_all_records())
-    if not df.empty:
-        engineers = df['Не учитывать'].iloc[0].split(', ')                                #TODO избавиться от ', '
-        return engineers
-    else:
-        return None
-
-
-def get_list_of_engineers(df: DataFrame, colomn: str) -> list:
-    '''
-    Принимает датафрейм и название столбца,
-    из которого брать фамилии проектировщиков.
-    Возвращает список проектировщиков.
-    '''
-    engineers = set(df[colomn])
-    non_count_eng = non_count_engineers()
-    groups_of_engineers = set()
-    groups = set()
-
-    if '' in engineers:
-        engineers.remove('')
-
-    for engineer in engineers:
-        if ',' in engineer:
-            groups_of_engineers.add(engineer)
-
-    for group in groups_of_engineers:
-        groups.update(group.split(', '))
-        engineers.remove(group)
-    
-    union_eng = engineers.union(groups)
-
-    if not non_count_eng:
-        return list(union_eng)
-    else:
-        unique_eng = union_eng - set(non_count_eng)
-        return list(unique_eng)
-
-
-def count_block(row: Series, blocks: list) -> float:
-    '''
-    Расчитывает баллы для блок-контейнеров.
-    За первый разработанный объект - 1 балл,
-    за остальные - 0.5 балла.
-    '''
-    name = row['Наименование объекта']
-
-    if name in blocks:
-        return 0.5
-    else:
-        blocks.append(name)
-        return 1.0
-
-
-
-def count_points(row: Series, df: DataFrame, blocks: list) -> int:
-    '''
-    Считает и возвращает сумму полученных баллов.
-    Если подсчет произвести невозможно, то возвращает
-    строку-предупреждение об этом.
-    '''
-    points = 0
-    filled_project = check_filled_projects(row)
-    if not filled_project:
-        return 'Необходимо заполнить данные для расчёта'
-    if 'блок-контейнер' in row['Тип объекта'].strip().lower():
-        return count_block(row, blocks)
-    try:
-        complexity = int(row['Сложность'])
-    except ValueError:
-        return 'Сложность объекта заполнена некорректно'
-
-    points += check_amount_directions(complexity, row['Количество направлений'])
-    points += check_square(complexity, row)
-    points += check_sot_skud(row)
-    points += check_cultural_heritage(row)
-    points += check_net(row)
-    points = round(points/check_authors(row['Разработал']),1)
-    points = check_spend_time(row, points, df)
-
-    return points
-
-
-def points_for_adjusting(row: Series, engineer: str):
-    '''
-    Расчитывает баллы за корректировки: 0.3 от баллов за готовый проект, не считая дедлайн.
-    '''
-    points = 0
-    filled_project = check_filled_projects(row)
-
-    if engineer in row['Разработал']:
-        return 'Баллы за корректировки своих проектов не расчитываются'
-
-    if not filled_project:
-        return 'Необходимо заполнить данные для расчёта'
-    if 'блок-контейнер' in row['Тип объекта'].strip().lower():
-        return 0.3
-    
-    try:
-        complexity = int(row['Сложность'])
-    except ValueError:
-        return 'Сложность объекта заполнена некорректно'
-
-    points += check_amount_directions(complexity, row['Количество направлений'])
-    points += check_square(complexity, row)
-    points += check_sot_skud(row)
-    points += check_cultural_heritage(row)
-    points += check_net(row)
-    points = round(points/check_authors(row['Корректировки']),1)
-
-    return points*0.3
-
-
-
-def count_points_for_adjusting(df: DataFrame) -> Worksheet:
+def process_adjusting_data(df: DataFrame) -> Worksheet:
     '''
     Расчитывает баллы за корректировки и отсылает данные в таблицу "Премирование".
     '''
@@ -174,28 +30,16 @@ def count_points_for_adjusting(df: DataFrame) -> Worksheet:
     if eng_for_adj != []:
         for engineer in eng_for_adj:
             adjusting_projects = df.loc[df['Корректировки'].str.contains(f'{engineer}')].reset_index(drop=True)
-            adjusting_projects["Баллы"] = adjusting_projects.apply(points_for_adjusting, axis=1, args=(adjusting_projects, engineer))
+            adjusting_projects["Баллы"] = adjusting_projects.apply(count_adjusting_points, axis=1, args=(adjusting_projects, engineer))
             adjusting_projects_small = adjusting_projects[['Наименование объекта', 'Шифр (ИСП)', 'Разработал', 'Баллы',
                     'Корректировки']]
             send_adj_data_to_spreadsheet(adjusting_projects_small, engineer)
 
 
-def is_point(s: str) -> bool:
-    '''
-    Проверка столбца "Баллы".
-    Если значение состоит из строки, в котрой только число, возвращает True.
-    '''
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-
-
 def process_data(engineers: list[str], df: DataFrame) -> None:
     '''
     Собирает данные из архива проектов, производит расчет баллов
-    и отправляет полученные данные в новую таблицу.
+    и отправляет полученные данные в таблицу "Премирование".
     '''
     for engineer in engineers:
         print(engineer)
@@ -205,10 +49,6 @@ def process_data(engineers: list[str], df: DataFrame) -> None:
         engineer_projects["Баллы"] = engineer_projects.apply(count_points, axis=1, args=(engineer_projects, blocks))
         send_project_data_to_spreadsheet(engineer_projects, engineer)
 
-
-        # engineer_projects_filtered = engineer_projects[[
-        #     'Шифр (ИСП)', 'Разработал', 'Дата начала проекта', 'Дата окончания проекта', 'Баллы'
-        # ]].loc[engineer_projects['Баллы'] != 'Необходимо заполнить данные для расчёта']
 
         engineer_projects_filt = engineer_projects[engineer_projects['Баллы'].apply(is_point)]
 
@@ -222,11 +62,10 @@ def process_data(engineers: list[str], df: DataFrame) -> None:
             send_quarter_data_to_spreadsheet(quarters, engineer)
         time.sleep(20)
     
-    count_points_for_adjusting(df)
+    process_adjusting_data(df)
         
 
 async def main():
-    gc = gspread.service_account(filename=creds_path)
     bot = aiogram.Bot(token=TELEGRAM_TOKEN)
     
     try:
@@ -251,7 +90,23 @@ async def main():
                 await send_message(bot, f'Ошибка: {error}')
     
     await bot.session.close()
-    
+
+
+def setup_scheduler():
+    '''
+    Запускает планировщик.
+    '''
+    scheduler = AsyncIOScheduler()
+
+    scheduler.add_job(main, trigger='cron', hour=10, minute=10)
+
+    scheduler.start()
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    setup_scheduler()
+
+    try:
+        asyncio.get_event_loop().run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        pass
