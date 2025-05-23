5e2643925db04b8ea1a07d1a5f202329eb8aa338
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-05-23 15:43:16 +0400

    fix/add %(funcName)s in logger

--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 import logging
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s',
     level=logging.INFO
 )
