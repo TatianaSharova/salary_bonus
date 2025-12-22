a63c48a2634efe1d1e24178bb4cc8e6a35be2941
Tatiana Sharova <kroxxatt@gmail.com>
2025-12-22 23:59:27 +0400

    take out process funcs

--- src/salary_bonus/main.py
+++ src/salary_bonus/main.py
@@ -8,153 +8,23 @@ from datetime import datetime, timedelta
 import pandas as pd
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
-from pandas.core.frame import DataFrame
 from pytz import timezone
 
 sys.path.insert(
     0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 )
 
-from src.salary_bonus.calculations.complexity import set_project_complexity
-from src.salary_bonus.calculations.counting_points import count_points
-from src.salary_bonus.calculations.mounth_points import calculate_by_month
-from src.salary_bonus.calculations.results import do_results
-from src.salary_bonus.logger import logging
-from src.salary_bonus.notification.telegram.bot import send_tg_message, tg_bot
-from src.salary_bonus.utils import get_employees, get_project_archive_data, is_point
-from src.salary_bonus.worksheets.google_sheets_manager import sheets_manager
-from src.salary_bonus.worksheets.worksheets import (
-    connect_to_engineer_ws,
-    send_project_data_to_spreadsheet,
-    send_quarter_data_to_spreadsheet,
+from src.salary_bonus.calculations.lead_results import process_lead_data
+from src.salary_bonus.calculations.project_archive.process import (
+    process_project_archive_data,
 )
+from src.salary_bonus.logger import logging
+from src.salary_bonus.notification.telegram.bot import TelegramNotifier
+from src.salary_bonus.utils import get_employees, get_project_archive_data
+from src.salary_bonus.worksheets.google_sheets_manager import sheets_manager
 
 pd.options.mode.chained_assignment = None
 pd.set_option("future.no_silent_downcasting", True)
-
-
-def correct_complexity(engineer: str, engineer_projects: DataFrame) -> DataFrame:
-    """
-    Берёт данные о корректировке сложности из таблицы проектировщика
-    и заменяет неверные данные.
-    """
-    logging.info("Проверка на необходимость корректировки сложности проектов.")
-    worksheet = connect_to_engineer_ws(engineer)
-
-    if worksheet:
-        raw_data = worksheet.get("J1:J200")
-        new_coplexity = pd.DataFrame(raw_data[1:], columns=raw_data[0])
-        if not new_coplexity.empty:
-            engineer_projects["Корректировка сложности"] = new_coplexity[
-                "Корректировка сложности"
-            ]
-            if "Корректировка сложности" in engineer_projects.columns:
-                engineer_projects["Сложность для расчета"] = engineer_projects[
-                    "Корректировка сложности"
-                ].combine_first(engineer_projects["Сложность для расчета"])
-                logging.info(
-                    "Скорректировали сложность проектов "
-                    "по данным с листа проектировщика."
-                )
-        logging.info("Корректировка сложности не нужна.")
-    return engineer_projects
-
-
-def find_sum_equipment(df: DataFrame) -> DataFrame:
-    """
-    Считает сумму заложенного оборудования по кварталам.
-    """
-    logging.info("Начинаем подсчет суммы заложенного оборудования по кварталам.")
-    name = "Сумма заложенного оборудования"
-    df[name] = df[name].str.replace("\xa0", "").str.replace(",", ".")
-    df[name] = pd.to_numeric(df[name], errors="coerce")
-
-    equipment_df = df[
-        [
-            "Шифр (ИСП)",
-            "Разработал",
-            "Дата начала проекта",
-            "Дата окончания проекта",
-            "Сумма заложенного оборудования",
-        ]
-    ]
-    equipment_df_filt = equipment_df.dropna(subset=[name])
-    equipment_df_filt = equipment_df_filt[
-        equipment_df_filt["Шифр (ИСП)"] != ""
-    ]  # noqa: E501
-
-    quaters = calculate_by_month(equipment_df_filt, column=name)
-    quaters[name] = quaters[name].apply(lambda x: "{:,.2f}".format(x).replace(",", " "))
-    return quaters
-
-
-def process_project_archive_data(engineers: list[str], df: DataFrame) -> None:
-    """
-    Собирает данные из архива проектов, производит расчет баллов
-    и отправляет полученные данные в таблицу "Премирование"
-
-    Для тех проектировщиков, для которых удалось посчитать баллы за проекты,
-    выводится информация с данными о баллах по кварталам на его листе,
-    а также происходит сбор информации о рабочих часах с листа посещаемости.
-    """
-    results = {}
-    engineers = ["Фокина"]
-
-    for engineer in engineers:
-        logging.info(f"Начинается расчет баллов для проектировщика {engineer}.")
-        blocks = []
-        engineer_projects = df.loc[
-            df["Разработал"].str.contains(f"{engineer}")
-        ].reset_index(drop=True)
-        engineer_projects["Дедлайн"] = ""
-
-        logging.info("Определение сложности проектов.")
-        engineer_projects["Автоматически определенная сложность"] = (
-            engineer_projects.apply(set_project_complexity, axis=1)  # noqa: E501
-        )
-        engineer_projects["Сложность для расчета"] = engineer_projects[
-            "Автоматически определенная сложность"
-        ]
-        engineer_projects = correct_complexity(engineer, engineer_projects)
-
-        logging.info("Подсчет баллов за проекты.")
-        engineer_projects["Баллы"] = engineer_projects.apply(
-            count_points, axis=1, args=(engineer_projects, blocks)
-        )
-        send_project_data_to_spreadsheet(engineer_projects, engineer)
-
-        engineer_projects_filt = engineer_projects[
-            engineer_projects["Баллы"].apply(is_point)
-        ]
-
-        engineer_projects_filtered = engineer_projects_filt[
-            [
-                "Шифр (ИСП)",
-                "Разработал",
-                "Дата начала проекта",
-                "Дата окончания проекта",
-                "Баллы",
-            ]
-        ]
-
-        if not engineer_projects_filtered.empty:
-            quarters = calculate_by_month(engineer_projects_filtered, column="Баллы")
-            send_quarter_data_to_spreadsheet(quarters, engineer)
-            results[engineer] = quarters
-            logging.info(
-                f"Расчет баллов для проектировщика {engineer} завершен. "
-                f"Ждем 10 секунд."
-            )
-        else:
-            logging.info(
-                f"Нет готовых проектов у проектировщика {engineer}. "
-                f"Переходим к следующему проектировщику через 10 секунд."
-            )
-        # time.sleep(10)
-
-    sum_equipment = find_sum_equipment(df)
-
-    do_results(results, sum_equipment)
 
 
 async def main() -> None:
@@ -162,51 +32,103 @@ async def main() -> None:
     Запускает и завершает работу программы.
     """
     logging.info("Запущена основная задача.")
+    tg_bot = TelegramNotifier()
 
     df = await get_project_archive_data()
 
-    if df.empty:
+    if df is None:
+        await tg_bot.send_message(
+            'Ошибка: таблица "Таблица проектов" не найдена.\n'
+            "Возможно название было сменено.",
+        )
         sheets_manager.invalidate()
-        await tg_bot.session.close()
+        await tg_bot.close()
+        return
+    elif isinstance(df, pd.DataFrame) and df.empty:
+        msg = "Таблица проектов пуста, расчет не будет произведен."
+        logging.warning(msg)
+        await tg_bot.send_message(msg)
+        sheets_manager.invalidate()
+        await tg_bot.close()
         return
 
     try:
         employees_data = get_employees()
         list_of_engineers = employees_data["engineers"]
 
+        # Рассчет баллов по основным проектам для проектировщиков
         if len(list_of_engineers) > 0:
-            process_project_archive_data(list_of_engineers, df)
-            await send_tg_message(tg_bot, "Расчет баллов успешно выполнен.")
-            logging.info("Программа успешно выполнила работу.")
+            archive_points = process_project_archive_data(list_of_engineers, df)
         else:
-            msg = "Нет данных о проектировщиках для расчета. Расчет не будет произведен."
+            msg = (
+                "Нет данных о проектировщиках для расчета на листе 'Настройки'."
+                " Расчет не будет произведен."
+            )
             logging.warning(
                 msg + f"\n\nПолученные данные с листа 'Настройки':\n {employees_data}"
             )
-            await send_tg_message(tg_bot, msg)
+            await tg_bot.send_message(msg)
+            sheets_manager.invalidate()
+            await tg_bot.close()
+            return
+
+        # Рассчет баллов по дополнительным проектам для проектировщиков
+        # points = process_additional_work_data(archive_points, list_of_engineers)
+        # TODO
+
+        # Рассчет баллов для руководителей
+        if len(list(employees_data["lead"].keys())) > 0:
+            process_lead_data(archive_points, employees_data["lead"])
+        else:
+            msg = (
+                "Баллы проектировщиков посчитаны и отправлены на листы проектировщиков."
+                " Для расчета баллов руководителей не найдено данных."
+            )
+            logging.warning(msg)
+            await tg_bot.send_message(msg)
+            sheets_manager.invalidate()
+            await tg_bot.close()
+
+        gip = employees_data["chief"]
+        if len(gip) > 0:
+            ...
+        else:
+            msg = (
+                "Баллы для руководителей посчитаны и отправлены на листы руководителей. "
+                " Для расчета баллов главных инженеров не найдено данных."
+            )
+            logging.warning(msg)
+            await tg_bot.send_message(msg)
+            sheets_manager.invalidate()
+            await tg_bot.close()
+
+        await tg_bot.send_message("Расчет баллов окончен.")
     except Exception as error:
         logging.exception(error)
         error_name = type(error).__name__
         tb = "".join(traceback.format_tb(error.__traceback__))
 
-        await send_tg_message(
-            tg_bot, f"Во время расчета произошла ошибка {error_name}: {error}\n\n" f"{tb}"
+        await tg_bot.send_message(
+            f"Во время расчета произошла ошибка {error_name}: {error}\n\n" f"{tb}"
         )
     finally:
         sheets_manager.invalidate()
-        await tg_bot.session.close()
+        await tg_bot.close()
 
 
 async def update_holidays_package():
     """
     Обновляет пакет holidays для подгрузки данных о выходных в новых годах.
     """
+    tg_bot = TelegramNotifier()
     try:
         subprocess.run(["pip", "install", "--upgrade", "holidays"], check=True)
         logging.info("Библиотека holidays была обновлена.")
     except subprocess.CalledProcessError as error:
         logging.exception(f"Библиотека holidays не обновлена: {error}")
-        await send_tg_message(tg_bot, f"Ошибка при обновлении holidays library: {error}")
+        await tg_bot.send_message(f"Ошибка при обновлении holidays library: {error}")
+    finally:
+        await tg_bot.close()
 
 
 def setup_scheduler():
